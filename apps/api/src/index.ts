042b91df93de1bf6d9fa8d9c803d55d5a2cdf44f
stylex86 <williams.paredes.86@gmail.com>
2024-03-18 12:35:32 -0300

    se separa index y se deja como app para despues realizar los testing unitarios

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,21 +1,4 @@
-import fastify, { FastifyInstance} from 'fastify';
-import fastifyCookie from '@fastify/cookie';
-import albumRoute from './routes/album.routes';
-import dotenv from 'dotenv';
-const app: FastifyInstance = fastify({ logger: true });
-
-import './utils/mongoose';
-dotenv.config();
-
-
-app.register(fastifyCookie);
-app.register(albumRoute, { prefix: '/api/v1' });
-
-// Declara una ruta
-app.get('/', function (request, reply) {
-  reply.clearCookie('access_token');
-  return request.cookies;
-});
+import app from './app'
 
 // Ejecuta el servidor
 app.listen({ host: '0.0.0.0', port: 3000 }, function (err, address) {
